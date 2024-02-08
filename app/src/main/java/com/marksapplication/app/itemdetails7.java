f25935b5eab874111d9578b72a331d373fd8460e
Ogarii <mbonuke33@gmail.com>
2024-02-08 04:24:01 +0300

    Added the imageDetails *Fix The Date

--- app/src/main/java/com/marksapplication/app/itemdetails7.java
+++ app/src/main/java/com/marksapplication/app/itemdetails7.java
@@ -2,13 +2,83 @@ package com.marksapplication.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.firebase.Timestamp;
+
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class itemdetails7 extends AppCompatActivity {
+
+    ImageView imageView7,imageArrowLeftOne;
+    TextView prodname,prodPrice,prodCategorry,prodquantitty,txtIDescription;
+
+    String purchasedate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android_small_seven);
+
+        imageView7 = findViewById(R.id.imageView7);
+        prodname = findViewById(R.id.prodname);
+        prodPrice = findViewById(R.id.prodPrice);
+        prodCategorry = findViewById(R.id.prodCategoryy);
+        prodquantitty = findViewById(R.id.prodquantityy);
+        txtIDescription = findViewById(R.id.txtIDescription);
+        imageArrowLeftOne = findViewById(R.id.imageArrowLeftOne);
+
+        imageArrowLeftOne.setOnClickListener(v -> {
+            finish();
+        });
+
+        // Get the intent that started this activity and extract the string
+
+        Intent intent = getIntent();
+
+        String name = intent.getStringExtra("productName");
+        String qty = intent.getStringExtra("productQuantity");
+        String sellingprice = intent.getStringExtra("productSellingPrice");
+        String category = intent.getStringExtra("productCategory");
+        String image = intent.getStringExtra("productImage");
+        purchasedate = intent.getStringExtra("purchaseDate");
+
+        String formattedDate = null;
+        if (purchasedate != null) {
+            try {
+                long purchaseTimeInMillis = Long.parseLong(purchasedate);
+                Date date = new Date(purchaseTimeInMillis); // Assuming you have date in milliseconds
+                SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy"); // Customize format as needed
+                formattedDate = formatter.format(date);
+            } catch (NumberFormatException e) {
+                e.printStackTrace(); // Log the exception for debugging
+            }
+        }
+
+
+        String buyingprice = intent.getStringExtra("productBuyingPrice");
+        String code = intent.getStringExtra("productCode");
+
+        prodname.setText("Product: " +name);
+        prodquantitty.setText("Quantity: " +qty);
+        prodPrice.setText("SellingPrice: " + sellingprice);
+        prodCategorry.setText("Category: " +category);
+        txtIDescription.setText(String.format("Product Code: %s\nPurchased Date: %s\nBuying Price: %s", code, formattedDate , buyingprice));
+
+        if (image != null) {
+            imageView7.setImageURI(Uri.parse(image));
+        } else {
+            Toast.makeText(this, "No image found", Toast.LENGTH_SHORT).show();
+            // Set a placeholder image or handle the absence of an image URI accordingly
+        }
+
+
+
     }
 }
--- app/src/main/java/com/marksapplication/app/itemview8.java
+++ app/src/main/java/com/marksapplication/app/itemview8.java
@@ -8,6 +8,7 @@
     import androidx.recyclerview.widget.RecyclerView;
 
     import android.content.Intent;
+    import android.net.Uri;
     import android.os.Bundle;
     import android.util.Log;
     import android.view.LayoutInflater;
@@ -16,6 +17,7 @@
     import android.widget.ImageView;
     import android.widget.LinearLayout;
     import android.widget.TextView;
+    import android.widget.Toast;
 
     import com.google.android.gms.tasks.OnFailureListener;
     import com.google.android.gms.tasks.OnSuccessListener;
@@ -109,7 +111,7 @@
         // Define your ItemAdapter class here
         private static class ItemAdapter extends RecyclerView.Adapter<itemview8.ItemAdapter.ItemViewHolder> {
             private List<Items> itemsList;
-            private itemview8 context;
+            private static itemview8 context;
 
             public ItemAdapter(itemview8 context, List<Items> itemsList) {
                 this.context = context;
@@ -138,23 +140,64 @@
 
                 ImageView imageView4;
                 TextView prodName,prodQuantity,prodPrice,prodCategory;
+                LinearLayout linearRowrectangletwentytwo;
 
                 ItemViewHolder(@NonNull View itemView) {
                     super(itemView);
 
                     imageView4 = itemView.findViewById(R.id.imageView4);
                     prodName = itemView.findViewById(R.id.prodName);
-                    prodQuantity = itemView.findViewById(R.id.prodQunatity);
+                    prodQuantity = itemView.findViewById(R.id.prodname);
                     prodPrice = itemView.findViewById(R.id.prodPrice);
                     prodCategory = itemView.findViewById(R.id.prodCategoryy);
+                    linearRowrectangletwentytwo = itemView.findViewById(R.id.linearRowrectangletwentytwo);
+
+
+
                 }
 
                 public void bind(Items items) {
+
 
                     prodName.setText(items.getProductName());
                     prodQuantity.setText(items.getProductQuantity());
                     prodPrice.setText(items.getProductSellingPrice());
                     prodCategory.setText(items.getProductCategory());
+
+                    if (items.getImageUri() != null) {
+                        imageView4.setImageURI(Uri.parse(items.getImageUri()));
+                    } else {
+                        Toast.makeText(itemView.getContext(), "No image found", Toast.LENGTH_SHORT).show();
+                        // Set a placeholder image or handle the absence of an image URI accordingly
+                    }
+
+                    linearRowrectangletwentytwo.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+
+                            Intent intent = new Intent(itemView.getContext(),itemdetails7.class);
+
+                            intent.putExtra("productName",items.getProductName());
+                            intent.putExtra("productQuantity",items.getProductQuantity());
+                            intent.putExtra("productSellingPrice",items.getProductSellingPrice());
+                            intent.putExtra("productCategory",items.getProductCategory());
+                            intent.putExtra("productImage",items.getImageUri());
+                            if (items.getDateTime() != null) {
+                                intent.putExtra("purchaseDate", items.getDateTime().toString());
+                                Log.d(TAG, "DateTime: " + items.getDateTime().toString());
+                            }
+                            else {
+                                Log.e(TAG, "DateTime is null");
+                            }
+                            intent.putExtra("purchaseDate",items.getDateTime().toString());
+                            intent.putExtra("productBuyingPrice",items.getProductBuyingPrice());
+                            intent.putExtra("productCode",items.getProductCode());
+
+
+
+                            context.startActivity(intent);
+                        }
+                    });
 
 
                     // Bind data to your item views here
